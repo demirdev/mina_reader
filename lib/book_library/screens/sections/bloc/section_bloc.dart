39a910cec1691de8634997eb6746bcdc89722c16
mehmet <mdemir85@icloud.com>
2021-10-12 22:05:52 +0300

    added SectionLoading state, Get.to replaced as Navigator.push

--- lib/book_library/screens/sections/bloc/section_bloc.dart
+++ lib/book_library/screens/sections/bloc/section_bloc.dart
@@ -2,12 +2,10 @@ import 'dart:async';
 
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get/get.dart';
 import 'package:mina_reader/book_library/model/book.dart';
-import 'package:mina_reader/book_library/screens/sections/helper/name_parsing_helper.dart';
+import 'package:mina_reader/extensions/extensions_future.dart';
 
 import '../../../../mina_reader.dart';
-import '../../reader/book_reader_screen.dart';
 
 part 'section_event.dart';
 part 'section_state.dart';
@@ -15,12 +13,17 @@ part 'section_state.dart';
 class SectionBloc extends Bloc<SectionEvent, SectionState> {
   final Book book;
 
-  SectionBloc(this.book) : super(SectionInitial());
+  SectionBloc(this.book) : super(SectionInitial()) {
+    400.msAfter(run: () {
+      gotoLastReadingSection();
+    });
+  }
 
   @override
   Stream<SectionState> mapEventToState(
     SectionEvent event,
   ) async* {
+    yield SectionLoading();
     if (event is SectionTapped) {
       yield* _mapSectionTappedToState(event);
     }
@@ -30,32 +33,19 @@ class SectionBloc extends Bloc<SectionEvent, SectionState> {
   }
 
   Stream<SectionState> _mapSectionTappedToState(SectionTapped event) async* {
-    gotoReadingScreen(event.sectionIndex, event.title);
+    yield SectionGoToReader(book: event.book, sectionIndex: event.sectionIndex);
+    saveLastReadedSection(event.sectionIndex);
   }
 
-  void gotoLastReadingSction() {
+  void gotoLastReadingSection() {
     final lastReadedSectionId =
         boxLastReadedSectionId.get(book.assetFolder, defaultValue: -1);
     if (lastReadedSectionId != -1) {
-      gotoReadingScreen(
-          lastReadedSectionId, book.sections[lastReadedSectionId].title ?? '');
-    }
-  }
-
-  void gotoReadingScreen(int sectionIndex, String title) {
-    saveLastReadedSection(sectionIndex);
-
-    Get.to(() => BookReaderScreen(
+      add(SectionTapped(
+          title: book.sections[lastReadedSectionId].title ?? '',
           book: book,
-          sectionIndex: sectionIndex,
-          onTapNextSection: () {
-            gotoReadingScreen(
-                sectionIndex + 1, book.sections[sectionIndex + 1].title ?? '');
-          },
-          nextSectionTitle: (sectionIndex + 1) >= book.sections.length
-              ? null
-              : getSectionTitle(sectionIndex + 1, book),
-        ));
+          sectionIndex: lastReadedSectionId));
+    }
   }
 
   void saveLastReadedSection(int index) {
@@ -64,6 +54,6 @@ class SectionBloc extends Bloc<SectionEvent, SectionState> {
 
   Stream<SectionState> _mapSectionGoToLastReadingRequiredToState(
       SectionGoToLastReadingRequired event) async* {
-    gotoLastReadingSction();
+    gotoLastReadingSection();
   }
 }
