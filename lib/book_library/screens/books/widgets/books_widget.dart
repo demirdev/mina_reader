fa5daa173fb5644100b3bc11d46840f8e36b2bef
mehmet <mdemir85@icloud.com>
2021-10-12 22:06:33 +0300

    BlocListener added

--- lib/book_library/screens/books/widgets/books_widget.dart
+++ lib/book_library/screens/books/widgets/books_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mina_reader/book_library/model/book.dart';
 import 'package:mina_reader/book_library/screens/books/bloc/book_bloc.dart';
+import 'package:mina_reader/book_library/screens/sections/SectionScreen.dart';
 
 import 'book_list_tile_widget.dart';
 
@@ -15,21 +16,39 @@ class BooksWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2, crossAxisSpacing: 4.0, mainAxisSpacing: 4.0),
-        itemBuilder: (context, index) {
-          return InkWell(
-            onTap: () {
-              BlocProvider.of<BookBloc>(context).add(BookTapped(books[index]));
-            },
-            child: BookListTileWidget(book: books[index]),
-          );
-        },
-        itemCount: books.length,
+    return BlocListener(
+      bloc: BlocProvider.of<BookBloc>(context),
+      listener: (BuildContext context, BookState state) {
+        if (state is BookGoToSection) {
+          if (state.bookType == BookTypes.normalBook) {
+            gotoSectionScreen(context, state);
+          }
+        }
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, crossAxisSpacing: 4.0, mainAxisSpacing: 4.0),
+          itemBuilder: (context, index) {
+            return InkWell(
+              onTap: () {
+                BlocProvider.of<BookBloc>(context)
+                    .add(BookTapped(books[index]));
+              },
+              child: BookListTileWidget(book: books[index]),
+            );
+          },
+          itemCount: books.length,
+        ),
       ),
+    );
+  }
+
+  void gotoSectionScreen(BuildContext context, BookGoToSection state) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SectionScreen(state.book)),
     );
   }
 }
