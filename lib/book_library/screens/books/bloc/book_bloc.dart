4d3684363dcd3daa189f9a8a1483287f87003422
mehmet <mdemir85@icloud.com>
2021-10-12 22:00:34 +0300

    added Loading, GotoSection states

--- lib/book_library/screens/books/bloc/book_bloc.dart
+++ lib/book_library/screens/books/bloc/book_bloc.dart
@@ -2,23 +2,22 @@ import 'dart:async';
 
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get/get.dart';
 import 'package:mina_reader/book_library/model/book.dart';
 import 'package:mina_reader/book_library/model/book_list.dart';
 
 import '../../../../mina_reader.dart';
-import '../../sections/SectionScreen.dart';
 
 part 'book_event.dart';
 part 'book_state.dart';
 
 class BookBloc extends Bloc<BookEvent, BookState> {
-  BookBloc() : super(BookInitial()) {}
+  BookBloc() : super(BookInitial());
 
   @override
   Stream<BookState> mapEventToState(
     BookEvent event,
   ) async* {
+    yield BookLoading();
     if (event is BookTapped) {
       yield* _mapBookTappedToState(event);
     }
@@ -26,27 +25,10 @@ class BookBloc extends Bloc<BookEvent, BookState> {
 
   Stream<BookState> _mapBookTappedToState(BookTapped event) async* {
     final book = event.book;
-
     if (book.title == SampleRisaleBookList.littleBooksLabel) {
-      gotoLittleBooks(book);
+      yield BookGoToSection(bookType: BookTypes.littleBook, book: book);
     } else {
-      gotoSectionScreen(book);
+      yield BookGoToSection(bookType: BookTypes.normalBook, book: book);
     }
-  }
-
-  void gotoSectionScreen(Book book) {
-    Get.to(() => SectionScreen(book));
-  }
-
-  void gotoLittleBooks(Book book) {
-    Get.to(
-      SectionScreen(
-        Book(
-          sections: book.sections,
-          assetFolder: book.assetFolder,
-          title: SampleRisaleBookList.littleBooksLabel,
-        ),
-      ),
-    );
   }
 }
